2a85f24b409129a07e74c018d112929ef513cbba
farahzuot <farahz997@eng.hu.edu.jo>
2022-05-09 15:22:52 +0300

    logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
+from cmath import log
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def login_view(request):
@@ -16,6 +17,9 @@ def login_view(request):
     return render(request , 'accounts/login.html' , {})
 
 def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/login')
     return render(request , 'accounts/logout.html' , {})
 
 def register_view(request):
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,13 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles import views
-from accounts.views import login_view
+from articles.views import (
+    article_search_view,
+    article_create_view,
+    article_details_view,
+)
+from accounts.views import (
+    login_view,
+    logout_view,
+)
 urlpatterns = [
     path('', home_view),
-    path('articles/', views.article_search_view),
-    path('articles/create', views.article_create_view),
-    path('articles/<int:id>', views.article_details_view),
+    path('articles/', article_search_view),
+    path('articles/create', article_create_view),
+    path('articles/<int:id>', article_details_view),
     path('admin/', admin.site.urls),
     path('login/', login_view),
+    path('logout/', logout_view),
 ]
