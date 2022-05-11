fbf372327cdbbcaf4edf259015719c44deff67eb
farahzuot <farahz997@eng.hu.edu.jo>
2022-05-11 12:17:11 +0300

    forms

--- articles/views.py
+++ articles/views.py
@@ -3,7 +3,7 @@ from multiprocessing import context
 from django.shortcuts import render
 from articles.models import Article
 from django.contrib.auth.decorators import login_required
-
+from articles.forms import ArticleForm
 # Create your views here.
 
 def article_search_view(request):
@@ -22,19 +22,31 @@ def article_search_view(request):
 
 @login_required
 def article_create_view(request, id=None):
-    print(request.method)
-    article_obj = None
-    context = {}
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        article_obj = Article.objects.create(title= title, content= content)
-        context = {
-            'object': article_obj,
-            'created': True,
-            }
-
+    form = ArticleForm(request.POST or None)
+    context = {'form': form}
+    if form.is_valid():
+        article_object = form.save()
+        context['form'] = ArticleForm()
     return render(request , 'articles/create.html', context=context)
+
+
+# old version_
+# def article_create_view(request, id=None):
+#     form = ArticleForm()
+#     context = {'form': form}
+#     if request.method == 'POST':
+#         form = ArticleForm(request.POST)
+#         context['form'] = form
+#         if form.is_valid():
+#             title = form.cleaned_data.get('title')
+#             content = form.cleaned_data.get('content')
+#             article_obj = Article.objects.create(title= title, content= content)
+#             context = {
+#                 'object': article_obj,
+#                 'created': True,
+#                 }
+
+#     return render(request , 'articles/create.html', context=context)
 
 
 def article_details_view(request, id=None):
