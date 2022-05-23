459dfc7a8cd1474817413b4dedd6edb3243d4af2
farahzuot <farahz997@eng.hu.edu.jo>
2022-05-23 12:44:50 +0300

    edit models

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,10 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateField(auto_now_add=False,auto_now=False , null=True, blank=True)
