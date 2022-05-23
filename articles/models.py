f666568d063052687896eff500ac9f6814a3b531
farahzuot <farahz997@eng.hu.edu.jo>
2022-05-23 13:35:28 +0300

    slugField

--- articles/models.py
+++ articles/models.py
@@ -1,10 +1,17 @@
 from operator import mod
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True , null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     published = models.DateField(auto_now_add=False,auto_now=False , null=True, blank=True)
+
+    def save(self, *args , **kwargs):
+        if self.slug == None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
