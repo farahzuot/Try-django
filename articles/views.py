0e13e2ed84fdfdf7d88b05c9724bdc15a7644724
farahzuot <farahz997@eng.hu.edu.jo>
2022-05-25 15:21:37 +0300

    slug for urls

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 import imp
 from multiprocessing import context
 from django.shortcuts import render
+from django.http import Http404
 from articles.models import Article
 from django.contrib.auth.decorators import login_required
 from articles.forms import ArticleForm
@@ -49,10 +50,17 @@ def article_create_view(request, id=None):
 #     return render(request , 'articles/create.html', context=context)
 
 
-def article_details_view(request, id=None):
+def article_details_view(request, slug=None):
     article_obj = None
-    if id:
-        article_obj = Article.objects.get(id=id)
+    if slug:
+        try:
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+        except Article.MultipleObjectsReturned: # this will not happen for the way we build our server, but just extra info to return whatever when there is a repetition.
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
     context = {
         'object' : article_obj,
     }
