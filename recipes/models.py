82acfb34c6209ad8d76891cef1b43d34b8d753a0
farahzuot <farahz997@eng.hu.edu.jo>
2022-06-09 20:50:58 +0300

    admin.py stackinline

--- recipes/models.py
+++ recipes/models.py
@@ -15,9 +15,9 @@ class Recipe(models.Model):
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
-    description = models.TextField(blank=True, null=True)
     quantity = models.CharField(max_length=50) # 1 1/4
     unit = models.CharField(max_length=50) # punds, lbs , oz , gram etc..
+    description = models.TextField(blank=True, null=True)
     directions = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
